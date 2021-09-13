ee4c0c0fed4fc53982789a6d0c7864ae7ad15ed2
Nick Nguyen <55971813+ninhdangcap123@users.noreply.github.com>
2021-09-13 11:21:53 +0700

    Fix Controllers
    Update Controllers

--- App/Controller/Course/Course_Controller.php
+++ App/Controller/Course/Course_Controller.php
@@ -1,53 +1,56 @@
 <?php
 
-include('App/Model/Course/Course_Model.php');
-$obj=new Course_Model();
-if (isset($_GET['action']))
-{
- $action = $_GET['action'];
-}
-else {
- $action = '';
-}
+  include('App/Model/Course/Course.php');
+  $obj=new Course_Model();
 
-switch ($action) {
-
-
-  case 'add-course':
-    if(isset($_POST['submit']))
+  if (isset($_GET['action']))
     {
-        $obj->create_course($_POST['course-short'],$_POST['course-full'],$_POST['cdate']);
+      $action = $_GET['action'];
     }
-    include "App/View/pages/Course/add-course.php";
-    break;
+  else 
+    {
+      $action = '';
+    }
+  switch ($action) 
+    {
+      case 'add-course':
+        if(isset($_POST['submit']))
+          {
+              $obj->create_course($_POST['course-short'],$_POST['course-full'],$_POST['cdate']);
+              echo "<script>alert('Course Added Successfully')</script>";
+          }
+        include "App/View/pages/Course/add-course.php";
+        break;
 
-    case 'view-course':
-      $rs=$obj->showCourse();
-      if(isset($_GET['del']))
-      {
-            $obj->del_course(intval($_GET['del']));
-      }
-      include "App/View/pages/Course/view-course.php";
-      break;
+      case 'view-course':
+        $rs=$obj->showCourse();
+        if(isset($_GET['del']))
+          {
+              $obj->del_course(intval($_GET['del']));
+              echo "<script>alert('Course has been deleted')</script>";
+              echo "<script>window.location.href='index.php?Controller=Course&action=view-course'</script>";
+          }
+        include "App/View/pages/Course/view-course.php";
+        break;
 
       case 'edit-course':
         $id=$_GET['cid'];
         $rs=$obj->showCourse1($id);
         $res=$rs->fetch_object();
         if(isset($_POST['submit']))
-        {
-            $obj->edit_course($_POST['course-short'],$_POST['course-full'],$_POST['udate'],$id);
-        }
+          {
+              $obj->edit_course($_POST['course-short'],$_POST['course-full'],$_POST['udate'],$id);
+              echo '<script>';
+              echo 'alert("Course Updated Successfully")';
+              echo '</script>';
+              echo "<script>window.location.href='index.php?Controller=Course&action=view-course'</script>";
+          }
         include "App/View/pages/Course/edit-course.php";
         break;
 
+      default:
+      include "App/View/pages/Course/add-course.php";
+      break;
+    }
 
-
-  default:
-  include "App/View/pages/Course/add-course.php";
-    break;
-}
-
-
-
- ?>
+?>
